4b078d41c93a0d9a8f5727095dfcf735118a0db6
Goy Shuh Xian <shuhxian@gmail.com>
2021-12-25 20:22:53 +0800

    Created placeholder java files

---
+++ app/src/main/java/com/example/self_enrichment_app/repository/GoalsTrackerRepository.java
@@ -0,0 +1,4 @@
+package com.example.self_enrichment_app.repository;
+
+public class GoalsTrackerRepository {
+}
---
+++ app/src/main/java/com/example/self_enrichment_app/viewmodel/GoalsTrackerViewModel.java
@@ -0,0 +1,4 @@
+package com.example.self_enrichment_app.viewmodel;
+
+public class GoalsTrackerViewModel {
+}
